e319a8cd4b80536771403259e8e815fcffe7f13d
jackt24 <48061691+jackt24@users.noreply.github.com>
2021-04-21 16:59:14 +0100

    dded the ability to change a items status based on the ID

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,6 +5,7 @@ from todo_app.data.session_items import *
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# I added the add functionality to the route rather than a seperate page as this seemed more efficient
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method == 'POST': 
@@ -15,6 +16,13 @@ def index():
 	else:
 		items = get_items()
 		return render_template("index.html", items=items)
+
+@app.route('/complete/<id>', methods=['POST', 'GET'])
+def complete(id):
+	completeItem = get_item(id)
+	completeItem['status'] = 'Complete'
+	save_item(completeItem)
+	return(id + ' marked as complete')
 
 if __name__ == '__main__':
     app.run()
