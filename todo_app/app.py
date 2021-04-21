f0287392be167d971716dc3fdc5422bbc71c7189
jackt24 <48061691+jackt24@users.noreply.github.com>
2021-04-21 15:01:48 +0100

    Here is a commit message

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,20 @@
-from flask import Flask
-
+from flask import Flask, render_template
+# from data.session_items as myData
 from todo_app.flask_config import Config
+from todo_app.data.session_items import *
+# from data import session_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/todo/list')
+def list():
+	items = get_items()
+	return render_template("index.html", items=items)
 
 if __name__ == '__main__':
     app.run()
