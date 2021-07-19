099c3d374a19d7403bd97a8d5c96f6b0cd169810
jackt24 <48061691+jackt24@users.noreply.github.com>
2021-07-19 11:41:53 +0100

    Put app into method and added test env

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,31 +3,34 @@ from todo_app.flask_config import Config
 from todo_app.data.session_items import *
 from todo_app.viewmodel import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+	app = Flask(__name__)
+	# app.config.from_object(Config)
 
-# Populate lists and add items 
-@app.route('/',methods=['POST', 'GET'])
-def index():
-	items = get_items()
-	complete_items = get_completeitems()
+	# Populate lists and add items 
+	@app.route('/',methods=['POST', 'GET'])
+	def index():
+		items = get_items()
+		complete_items = get_completeitems()
 
-	item_view_model = ViewModel(items)
-	
-	return render_template("index.html", view_model = item_view_model, complete_items=complete_items)
+		item_view_model = ViewModel(items)
+		
+		return render_template("index.html", view_model = item_view_model, complete_items=complete_items)
 
-# Add an item to the to do list
-@app.route('/add', methods=['POST', 'GET'])
-def add():
-	newItemTitle = request.form['text']
-	add_item(newItemTitle)
-	return redirect(url_for('index'))
+	# Add an item to the to do list
+	@app.route('/add', methods=['POST', 'GET'])
+	def add():
+		newItemTitle = request.form['text']
+		add_item(newItemTitle)
+		return redirect(url_for('index'))
 
-# Move an item from the todo list to the complete list
-@app.route('/complete/<id>')
-def complete(id):
-	complete_item(id)
-	return redirect(url_for('index'))
+	# Move an item from the todo list to the complete list
+	@app.route('/complete/<id>')
+	def complete(id):
+		complete_item(id)
+		return redirect(url_for('index'))
+
+	return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	create_app.run(debug=True)
