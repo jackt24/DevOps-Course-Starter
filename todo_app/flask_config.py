c68428549b54a5871af3384d6a9e54b06659bdd0
jackt24 <48061691+jackt24@users.noreply.github.com>
2021-07-19 14:56:32 +0100

    Changed config file and added base layer of integration tests

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -2,7 +2,16 @@ import os
 
 
 class Config:
-    """Base configuration variables."""
-    TRELLO_KEY = os.environ.get('TRELLO_KEY')
-    if not TRELLO_KEY:
-        raise ValueError("No TRELLO_KEY set for Flask application. Did you follow the setup instructions?")
+
+    def __init__(self):
+            self.TRELLO_BASE_URL = 'https://api.trello.com/1'
+            self.TRELLO_KEY = os.environ.get('TRELLO_KEY')
+            self.TOKEN = os.environ.get('TOKEN')
+            self.BOARD = os.environ.get('BOARD')
+            self.LIST = os.environ.get('LIST')
+            self.BOARD = os.environ.get('COMPLETELIST')
+
+    # """Base configuration variables."""
+    # TRELLO_KEY = os.environ.get('TRELLO_KEY')
+    # if not TRELLO_KEY:
+    #     raise ValueError("No TRELLO_KEY set for Flask application. Did you follow the setup instructions?")
