1f7d03f5cf1a4d6a9ae46c16d45ea73ccdfcd41b
jackt24 <48061691+jackt24@users.noreply.github.com>
2022-07-22 18:32:37 +0100

    Clean up

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,11 +4,5 @@ import os
 class Config:
 
     def __init__(self):
-            # self.TRELLO_BASE_URL = 'https://api.trello.com/1'
             self.MONGO_URL = os.getenv('MONGO_URL')
             self.MONGO_DB = os.getenv('MONGO_DB')
-            self.TRELLO_KEY = os.environ.get('TRELLO_KEY')
-            self.TOKEN = os.environ.get('TOKEN')
-            self.BOARD = os.environ.get('BOARD')
-            self.LIST = os.environ.get('LIST')
-            self.COMPLETELIST = os.environ.get('COMPLETELIST')
